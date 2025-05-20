342dc0eef0a87bf827c3f56f3170f1db5a918bbe
Rikelmy Roberto <50628746+rikelmyso7@users.noreply.github.com>
2025-05-20 16:57:41 -0300

    Update my_textfield.dart

--- lib/widgets/my_textfield.dart
+++ lib/widgets/my_textfield.dart
@@ -8,67 +8,57 @@ class MyTextField extends StatelessWidget {
   final String? Function(String?)? validator;
   final Widget? icon;
   final Widget? prefixicon;
-  final TextInputType? keyBordType;
+  final TextInputType? keyboardType;
+  final void Function(String)? onChanged;
+  final TextInputAction? textInputAction;
+  final List<TextInputFormatter>? inputFormatters;
 
   const MyTextField({
     Key? key,
     required this.controller,
     required this.hintText,
+    this.inputFormatters,
     this.obscureText = false,
     this.validator,
     this.icon,
-    this.keyBordType,
     this.prefixicon,
+    this.keyboardType,
+    this.onChanged,
+    this.textInputAction,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 70,
-            child: TextFormField(
-              controller: controller,
-              obscureText: obscureText,
-              validator: validator,
-              keyboardType: keyBordType,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey.shade300),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey.shade400),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                fillColor: Colors.white,
-                filled: true,
-                labelText: hintText,
-                labelStyle: MyTextStyle.hintTextFieldStyle,
-                prefixIcon: prefixicon,
-                prefixIconColor: Colors.black38,
-                suffixIcon: icon,
-              ),
+      child: Container(
+        height: 70,
+        child: TextFormField(
+          controller: controller,
+          obscureText: obscureText,
+          inputFormatters: inputFormatters,
+          validator: validator,
+          keyboardType: keyboardType,
+          onChanged: onChanged,
+          textInputAction: textInputAction,
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade300),
+              borderRadius: BorderRadius.circular(8),
             ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            fillColor: Colors.white,
+            filled: true,
+            labelText: hintText,
+            labelStyle: MyTextStyle.hintTextFieldStyle,
+            prefixIcon: prefixicon,
+            prefixIconColor: Colors.black38,
+            suffixIcon: icon,
           ),
-          Builder(
-            builder: (context) {
-              final errorText = (context
-                  .findAncestorStateOfType<FormFieldState<String>>()
-                  ?.errorText);
-              if (errorText != null) {
-                return Text(
-                  errorText,
-                  style: TextStyle(color: Colors.red, fontSize: 12),
-                );
-              }
-              return SizedBox(height: 0);
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
